e29f29c6016ea37b2104c2bb10f882249286c424
Clarisse Otmani <clarisseotm@gmail.com>
2024-07-18 16:12:07 +0200

    Created class TableViewController

--- src/main/java/fr/afpa/TableViewController.java
+++ src/main/java/fr/afpa/TableViewController.java
@@ -3,10 +3,10 @@ package fr.afpa;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class PrimaryController {
+public class TableViewController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-      
+
     }
 }
